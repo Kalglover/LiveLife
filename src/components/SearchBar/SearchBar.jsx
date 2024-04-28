b9028c1672e1e8f89ffd9fb949d8e639ca8e96de
Kalglover <111890647+Kalglover@users.noreply.github.com>
2024-04-28 18:57:38 -0400

    Co-authored-by: MWBradshaw <MWBradshaw@users.noreply.github.com>

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,32 +1,42 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
 import './SearchBar.css';
 
-const url = 'https://real-time-amazon-data.p.rapidapi.com/search?query=Phone&page=1&country=US&category_id=aps';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '39a603c7afmsh16537831fb0237fp1cd426jsn65962774609a',
-		'X-RapidAPI-Host': 'real-time-amazon-data.p.rapidapi.com'
-	  }
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-
-
 const SearchBar = () => {
-    return(
+    const [query, setQuery] = useState('');
+    const [listItems, setListItems] = useState(null);
+    const navigate = useNavigate(); 
+
+    const generateItems = (query) => {
+        const url = `https://real-time-amazon-data.p.rapidapi.com/search?query=${query}&page=1&country=US&category_id=aps`;
+
+        const options = {
+            method: 'GET',
+			headers: {
+				'X-RapidAPI-Key': '39a603c7afmsh16537831fb0237fp1cd426jsn65962774609a',
+				'X-RapidAPI-Host': 'real-time-amazon-data.p.rapidapi.com'
+			  }
+        };
+
+        fetch(url, options)
+            .then((response) => response.json())
+            .then(data => {
+                setListItems(data['data']['products']);
+                navigate('/search-results', { state: { listItems: data['data']['products'] } }); // Redirect using useNavigate
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }
+
+    return (
         <div id="search-bar">
-            <input type="text" placeholder="Search" id="search-bar-text" />
-            <button id="search-bar-button">
-                <img src="src/assets/icon _magnifying glass_.png" />
+            <input type="text" placeholder="Search" id="search-bar-text" onChange={(e) => setQuery(e.target.value)} />
+            <button id="search-bar-button" onClick={() => generateItems(query)}>
+                <img src="src/assets/icon _magnifying glass_.png" alt="Search" />
             </button>
         </div>
-    )
+    );
 }
 
 export default SearchBar;
